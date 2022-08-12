0f159a456916eccbcea69a68c52674377be5a3e5
Karl Moran <karlmoran06@gmail.com>
2022-08-12 11:22:49 +0000

    NavBarForm challenge, Copied over css

---
+++ my-app/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+import NavBarForm from "./components/NavBarForm";
+
+
+function App() {
+  return <div className="App">
+      <NavBarForm />
+  </div>;
+}
+
+export default App;
